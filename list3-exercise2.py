676bb8a5e871cafc426a835b01627def793f414a
brenda151295 <brenda151295@gmail.com>
2019-10-24 17:42:12 -0300

    exercise3

--- list3-exercise2.py
+++ list3-exercise2.py
@@ -1,7 +1,6 @@
 from scipy.optimize import minimize
 
 fun = lambda x: (0.25*x[0]*x[0]) + (x[1]*x[1])
-bnds = ((0, None), (0, None))
 x0 = [0.5,0]
 cons = ({'type': 'eq', 'fun': lambda x:  5 - x[0] - x[1]},
          {'type': 'ineq', 'fun': lambda x: x[0] + 0.2 * x[1] - 3})
